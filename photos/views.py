d63726e61f236ebaa198047f5da83275e0c22a12
Roney-juma <ochirone10@gmail.com>
2021-07-04 00:48:11 +0300

    past-photos view

--- photos/views.py
+++ photos/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 import datetime as dt
 from .models import Image
 
@@ -9,4 +9,20 @@ def todays_pics(request):
     date = dt.date.today()
     pics = Image.todays_pics()
 
-    return render (request, 'all-photos/recent_pics.html',{"date":date, "pics":pics})     
+    return render (request, 'all-photos/recent_pics.html',{"date":date, "pics":pics}) 
+
+#Function to redirect to photos posted in the past
+def past_pics (request, past_date):
+    #Convert date from the url string
+
+    try:
+        date = dt.datetime.strptime(past_date, '%Y-%m-%d').date()
+    except ValueError:
+        raise Http404()
+        assert False
+    
+    if date ==dt.date.today():
+        return redirect(todays_pics)
+
+    return render(request, 'all-photos/past_pics.html', {"date": date})
+    
