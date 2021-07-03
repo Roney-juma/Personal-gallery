d577cc4630ed6b762a4a1570bd3cda944cebb87f
Roney-juma <ochirone10@gmail.com>
2021-07-04 00:37:00 +0300

    work on models and views

--- photos/models.py
+++ photos/models.py
@@ -1,2 +1,11 @@
 from django.db import models
+import datetime as dt
 
+class Category(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+    def save_category(self):
+        self.save()
