d32eddb148c7280df7f0c8cc58efb133d74a1dd1
Roney-juma <ochirone10@gmail.com>
2021-07-04 00:34:51 +0300

    created nessesary html file

--- photos/views.py
+++ photos/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+import datetime as dt
+from .models import Image
 
 # Create your views here.
+#Function to display photos that have been posted today.
+def todays_pics(request):
+
+    date = dt.date.today()
+    pics = Image.todays_pics()
+
+    return render (request, 'all-photos/recent_pics.html',{"date":date, "pics":pics})     
