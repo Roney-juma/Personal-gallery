d0e20215c0fb5e46c26db3c8b12849643ed501ff
Roney-juma <ochirone10@gmail.com>
2021-07-04 23:05:32 +0300

    added class Image

--- photos/models.py
+++ photos/models.py
@@ -19,3 +19,22 @@ class Location (models.Model):
     
     def save_location(self):
         self.save()
+
+
+class Image (models.Model):
+    image = models.ImageField(upload_to = 'articles/', null=True)
+    image_name = models.CharField(max_length =30)
+    image_description = models.CharField(max_length =200)
+    image_location = models.ForeignKey(Location, on_delete=models.DO_NOTHING)
+    image_category = models.ForeignKey(Category, on_delete=models.DO_NOTHING)
+    pub_date = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return self.image_name
+
+    def save_image(self):
+        self.save()
+
+    def delete_image(self):
+        self.delete()
