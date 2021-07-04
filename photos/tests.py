0453f252eb30cc12d7c63d2637f0f87aa7892fe3
Roney-juma <ochirone10@gmail.com>
2021-07-04 23:23:11 +0300

    tested the save method

--- photos/tests.py
+++ photos/tests.py
@@ -12,3 +12,9 @@ class CategoryTestClass(TestCase):
         # Testing  instance
     def test_instance(self):
         self.assertTrue(isinstance(self.roney,Category))
+
+        # Testing Save Method
+    def test_save_method(self):
+        self.roney.save_category()
+        categories = Category.objects.all()
+        self.assertTrue(len(categories) > 0)
