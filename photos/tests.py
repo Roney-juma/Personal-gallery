4e2d9ce1e44aa6986f304e9790593a9da79516d1
Roney-juma <ochirone10@gmail.com>
2021-07-04 23:19:44 +0300

    testing the Category class

--- photos/tests.py
+++ photos/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from .models import Location,Image,Category
+
 
 # Create your tests here.
+class CategoryTestClass(TestCase):
+
+    # Set up method
+    def setUp(self):
+        self.roney= Category(name = 'Roney')
+
+        # Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.roney,Category))
