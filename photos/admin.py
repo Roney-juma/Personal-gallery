08986ac03d5b4e1326639d4fdd3297a289c3a475
Roney-juma <ochirone10@gmail.com>
2021-07-05 13:43:08 +0300

    added models to admin page

--- photos/admin.py
+++ photos/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Category,Location,Image
 
+admin.site.register(Category)
+admin.site.register(Location)
+admin.site.register(Image)
 # Register your models here.
